d02e1ae40454603cf0f28523c6c34be686c9b1f2
Runar Andre Myklebust <runar@rett24.no>
2020-02-13 21:28:52 +0100

    adding support for custom meta-tags

--- src/main/resources/site/processors/add-metadata.js
+++ src/main/resources/site/processors/add-metadata.js
@@ -7,10 +7,10 @@ var libs = {
 
 var view = resolve('add-metadata.html');
 
-exports.responseProcessor = function(req, res) {
+exports.responseProcessor = function (req, res) {
     var site = libs.portal.getSite();
     var content = libs.portal.getContent();
-	 var siteConfig = libs.common.getTheConfig(site);
+    var siteConfig = libs.common.getTheConfig(site);
 
     var isFrontpage = site._path === content._path;
     var pageTitle = libs.common.getPageTitle(content, site);
@@ -18,7 +18,7 @@ exports.responseProcessor = function(req, res) {
 
     var siteVerification = siteConfig.siteVerification || null;
 
-    var url = libs.portal.pageUrl({ path: content._path, type: "absolute" });
+    var url = libs.portal.pageUrl({path: content._path, type: "absolute"});
     var fallbackImage = siteConfig.seoImage;
     var fallbackImageIsPrescaled = siteConfig.seoImageIsPrescaled;
     if (isFrontpage && siteConfig.frontpageImage) {
@@ -31,17 +31,20 @@ exports.responseProcessor = function(req, res) {
         title: pageTitle,
         description: libs.common.getMetaDescription(content, site),
         siteName: site.displayName,
-        locale: libs.common.getLang(content,site),
+        locale: libs.common.getLang(content, site),
         type: isFrontpage ? 'website' : 'article',
         url: url,
         image: image,
         imageWidth: 1200, // Twice of 600x315, for retina
         imageHeight: 630,
-        blockRobots: siteConfig.blockRobots || libs.common.getBlockRobots(content),
+        blockRobots: siteConfig.blockRobots || libs.common.getBlockRobots(content),
         siteVerification: siteVerification,
         canonical: siteConfig.canonical,
-        twitterUserName : siteConfig.twitterUsername
+        twitterUserName: siteConfig.twitterUsername,
+        custom: setCustomValues(siteConfig.custom)
     };
+
+    log.info("ADD-METADATA-CUSTOM: %s", JSON.stringify(params, null, 4));
 
     var metadata = libs.thymeleaf.render(view, params);
 
@@ -50,12 +53,12 @@ exports.responseProcessor = function(req, res) {
     res.pageContributions.headEnd.push(metadata);
 
     // Handle injection of title - use any existing tag by replacing its content.
-	 // Also - Locate the <html> tag and make sure the "og" namespace is added.
+    // Also - Locate the <html> tag and make sure the "og" namespace is added.
     var titleHtml = '<title>' + pageTitle + titleAppendix + '</title>';
-	 var ogAttribute = 'og: http://ogp.me/ns#';
+    var ogAttribute = 'og: http://ogp.me/ns#';
     var titleAdded = false, ogAdded = false;
     if (res.contentType === 'text/html') {
-         if (res.body) {
+        if (res.body) {
             if (typeof res.body === 'string') {
                 // Find a title in the html and use that instead of adding our own title
                 var titleHasIndex = res.body.indexOf('<title>') > -1;
@@ -63,39 +66,43 @@ exports.responseProcessor = function(req, res) {
                     res.body = res.body.replace(/(<title>)(.*?)(<\/title>)/i, titleHtml);
                     titleAdded = true;
                 }
-					 // Find <html> and if it does not have proper "og"-prefix - inject it!
-					 var htmlIndex = res.body.toLowerCase().indexOf('<html');
-					 var endHtmlIndex = res.body.indexOf('>', htmlIndex);
-					 var thereIsAnAttributeThere = false;
-					 var tagAttributes = res.body.indexOf('=', htmlIndex);
-					 var prefixFound = false;
-					 if (tagAttributes) { thereIsAnAttributeThere = true; }
-					 if (thereIsAnAttributeThere) {
-						 var htmlTagContents = res.body.substr(htmlIndex+5, endHtmlIndex-htmlIndex-5).trim(); // Inside <html XX> - 5 is number of characters for <html
-						 var htmlTagAttributes = htmlTagContents.split("="); // Split on = so we can locate all the attributes.
+                // Find <html> and if it does not have proper "og"-prefix - inject it!
+                var htmlIndex = res.body.toLowerCase().indexOf('<html');
+                var endHtmlIndex = res.body.indexOf('>', htmlIndex);
+                var thereIsAnAttributeThere = false;
+                var tagAttributes = res.body.indexOf('=', htmlIndex);
+                var prefixFound = false;
+                if (tagAttributes) {
+                    thereIsAnAttributeThere = true;
+                }
+                if (thereIsAnAttributeThere) {
+                    var htmlTagContents = res.body.substr(htmlIndex + 5, endHtmlIndex - htmlIndex - 5).trim(); // Inside <html XX> - 5 is number of characters for <html
+                    var htmlTagAttributes = htmlTagContents.split("="); // Split on = so we can locate all the attributes.
 
-						 for (var i = 0; i < htmlTagAttributes.length; i++) {
-							if (htmlTagAttributes[i].toLowerCase().trim() === 'prefix') {
-								prefixFound = true;
-								if (htmlTagAttributes[i+1].indexOf(ogAttribute) === -1) {
-									//log.info("Before join - " + htmlTagAttributes[i+1]);
-									htmlTagAttributes[i+1] = htmlTagAttributes[i+1].substr(0,htmlTagAttributes[i+1].length-1) + ' ' + ogAttribute + htmlTagAttributes[i+1].substr(-1);
-									//log.info("After join - " + htmlTagAttributes[i+1]);
-								} else {
-									//log.info("Already in the tag!");
-								}
-							}
-						 }
-					 }
-					 // Join the new html element string, and create the new body to return.
-					 var fixedHtmlTag = htmlTagAttributes.join("=");
-					 if (!prefixFound) {
-						 fixedHtmlTag += ' prefix="' + ogAttribute + '"';
-					 }
-					 res.body = res.body.substr(0, htmlIndex+5)
-					 			 + ' '
-					 			 + fixedHtmlTag
-					 			 + res.body.substr(endHtmlIndex);
+                    for (var i = 0; i < htmlTagAttributes.length; i++) {
+                        if (htmlTagAttributes[i].toLowerCase().trim() === 'prefix') {
+                            prefixFound = true;
+                            if (htmlTagAttributes[i + 1].indexOf(ogAttribute) === -1) {
+                                //log.info("Before join - " + htmlTagAttributes[i+1]);
+                                htmlTagAttributes[i + 1] =
+                                    htmlTagAttributes[i + 1].substr(0, htmlTagAttributes[i + 1].length - 1) + ' ' + ogAttribute +
+                                    htmlTagAttributes[i + 1].substr(-1);
+                                //log.info("After join - " + htmlTagAttributes[i+1]);
+                            } else {
+                                //log.info("Already in the tag!");
+                            }
+                        }
+                    }
+                }
+                // Join the new html element string, and create the new body to return.
+                var fixedHtmlTag = htmlTagAttributes.join("=");
+                if (!prefixFound) {
+                    fixedHtmlTag += ' prefix="' + ogAttribute + '"';
+                }
+                res.body = res.body.substr(0, htmlIndex + 5)
+                           + ' '
+                           + fixedHtmlTag
+                           + res.body.substr(endHtmlIndex);
             }
         }
     }
@@ -107,7 +114,18 @@ exports.responseProcessor = function(req, res) {
         if (req.params.debug === 'true') {
             res.applyFilters = false; // Skip other filters
         }
-     }
+    }
 
     return res;
+};
+
+
+let setCustomValues = function (customKeyValuePairs) {
+    let kvPairs = [];
+
+    libs.util.data.forceArray(customKeyValuePairs).forEach(function (kvPair) {
+        kvPairs.push(kvPair);
+    });
+
+    return kvPairs;
 };
